7a224dbd4561de85c0957d4d5900f6f2357a6bb5
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-01 05:30:00 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# import library speech recognition
+import speech_recognition as sr 
+
+# buat variabel bicara dan panggil fungsi Recognizer
+bicara = sr.Recognizer()
+
+# dan panggil juga fungsi Microphone untuk mengenali audio melalui Microphone
+with sr.Microphone() as sumber:
+	print("Bicara Apapun : ")
+	audio = bicara.listen(sumber)
+	try:
+# buat variabel teks untuk menyimpan suara yang telah dikenal oleh google recognize
+		teks = bicara.recognize_google(audio)
+# print hasil audio menjadi teks
+		print("Kamu Mengatakan : {}".format(teks))
+	except:
+		print("Maaf,yang Kamu Bicarakan Tidak dimengerti")
